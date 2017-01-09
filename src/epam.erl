9ead443773c5d0e2f7d59366a4b8bd0a05340067
Badlop <badlop@process-one.net>
2017-01-09 16:48:48 +0100

    Update copyright year (processone/ejabberd#1442)

--- src/epam.erl
+++ src/epam.erl
@@ -5,7 +5,7 @@
 %%% Created : 5 Jul 2007 by Evgeniy Khramtsov <xram@jabber.ru>
 %%%
 %%%
-%%% epam, Copyright (C) 2002-2015   ProcessOne
+%%% epam, Copyright (C) 2002-2017   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
--- src/epam_sup.erl
+++ src/epam_sup.erl
@@ -5,7 +5,7 @@
 %%% Created : 4 Apr 2013 by Evgeniy Khramtsov <xram@jabber.ru>
 %%%
 %%%
-%%% epam, Copyright (C) 2002-2015   ProcessOne
+%%% epam, Copyright (C) 2002-2017   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
